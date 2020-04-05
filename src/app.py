d6101b18e4695162996f22cd515f3cbe43fbc798
chris <christianmatabaro@gmail.com>
2020-04-05 19:54:43 +0200

    make sure the program return the definition of words that start with a capital letter

--- src/app.py
+++ src/app.py
@@ -12,6 +12,9 @@ def searching(word):
     # checking if the word exist
     if word in data: 
         return data[word]
+
+    elif word.title() in data:
+        return data[word.title()]
     # checking for the word similarity and return a message
     elif len(get_close_matches(word, data.keys())) > 0 :
         # prompting the user to comfirm similarity check
